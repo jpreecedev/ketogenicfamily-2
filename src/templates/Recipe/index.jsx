cb20fab8ea7dc1401ef9e110b8b16dc7c2ca98ec
Jon Preece <jonpreece@Jons-MacBook-Pro.local>
2018-11-04 12:10:28 +0000

    Added ingredients for complex recipes

--- src/templates/Recipe/index.jsx
+++ src/templates/Recipe/index.jsx
@@ -10,6 +10,90 @@ import Recipe from '../../components/Recipe'
 
 import './styles.scss'
 
+function getRecipeIngredients(recipe) {
+  return recipe.ingredients.map(ingredient => {
+    let result = `"${ingredient.quantity} ${ingredient.units} ${ingredient.description}"`
+    if (ingredient.groupTitle) {
+      result = ingredient.ingredients.map(
+        groupedIngredient =>
+          `"${groupedIngredient.quantity} ${groupedIngredient.units} ${
+            groupedIngredient.description
+          }"`
+      )
+      result
+        .toString()
+        .trim()
+        .replace(/(^,)|(,$)/g, '')
+    }
+    return result
+  })
+}
+function getRecipeInstructions(recipe) {
+  const result = []
+  recipe.instructions.forEach((instruction, index) => {
+    result.push(`${index + 1}. ${instruction}`)
+  })
+  return result.join('\n')
+}
+function getNutrition(recipe) {
+  if (!recipe.hasNutritionalData) {
+    return ''
+  }
+
+  return `"nutrition": {
+      "@type": "NutritionInformation",
+      "servingSize": "1 ${recipe.servings.nutritionUnits}",
+      "calories": "${
+        recipe.nutrition.kcal ? recipe.nutrition.kcal.toFixed(0) : 0
+      } calories",
+      "fatContent": "${recipe.nutrition.fat ? recipe.nutrition.fat.toFixed(0) : 0} g",
+      "carbohydrateContent": "${
+        recipe.nutrition.carbohydrate ? recipe.nutrition.carbohydrate.toFixed(0) : 0
+      } g",
+      "fiberContent": "${
+        recipe.nutrition.fibre ? recipe.nutrition.fibre.toFixed(0) : 0
+      } g",
+      "proteinContent": "${
+        recipe.nutrition.protein ? recipe.nutrition.protein.toFixed(0) : 0
+      } g",
+      "saturatedFatContent": "${
+        recipe.nutrition.saturates ? recipe.nutrition.saturates.toFixed(0) : 0
+      } g",
+      "sodiumContent": "${
+        recipe.nutrition.salt ? recipe.nutrition.salt.toFixed(2) : 0
+      } g",
+      "sugarContent": "${
+        recipe.nutrition.sugars ? recipe.nutrition.sugars.toFixed(0) : 0
+      } g"
+    },`
+}
+
+function getRecipeStructuredData(recipe) {
+  return `{
+    "@context": "http://schema.org/",
+    "@type": "Recipe",
+    "name": "${recipe.title}",
+    "image": ["https://ketogenicfamily.com${recipe.imgSrc}"],
+    "author": {
+      "@type": "Person",
+      "name": "${recipe.author}"
+    },
+    "datePublished": "${recipe.published}",
+    "description": "${recipe.description}",
+    "recipeIngredient": [${getRecipeIngredients(recipe)}],
+    "recipeInstructions": "${getRecipeInstructions(recipe)}",
+    "recipeYield": "${recipe.servings.total}",
+    ${getNutrition(recipe)}
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingValue": "${recipe.rating}",
+      "ratingCount": "1"
+    },
+    "cookTime": "${recipe.cookTime ? recipe.cookTime : ''}",
+    "prepTime": "${recipe.prepTime}"
+  }`
+}
+
 function RecipeTemplate({ pageContext: { next, previous }, data: { recipesJson } }) {
   const recipe = recipesJson
   recipe.next = next
@@ -24,6 +108,11 @@ function RecipeTemplate({ pageContext: { next, previous }, data: { recipesJson }
         <div>
           <Recipe recipe={recipe} />
           <Disqus url={recipe.url} title={recipe.title} />
+          <script
+            hid="recipe-structured-data"
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: getRecipeStructuredData(recipe) }}
+          />
         </div>
       </div>
     </Layout>
@@ -61,6 +150,12 @@ export const pageQuery = graphql`
         units
         description
         groupTitle
+        ingredients {
+          key
+          quantity
+          units
+          description
+        }
       }
       instructions
     }
